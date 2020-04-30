0341ef0b88d20409892dad861b1f469306b43f56
Cross Zheng <zx@caiex.com>
2020-04-30 15:02:41 +0800

    :rocket: react progressive within iFrame.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-dashboard',
@@ -12,14 +13,27 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
+  // Insert IFrame URL
+  url = 'http://localhost:4200/assets/html/index.html';
+  ComponentUrl: SafeResourceUrl;
+
   constructor(
     private router: Router,
-    private heroService: HeroService) {
+    private heroService: HeroService,
+    
+    // Insert IFrame URL
+    private sanitizer: DomSanitizer
+    
+    ) {
   }
 
   ngOnInit(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+
+    // Insert IFrame URL
+    this.ComponentUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url)
+
   }
 
   gotoDetail(hero: Hero): void {
